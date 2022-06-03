c00aa4e61a3cf9a28423db83b08188abdf6f78c7
XavierEyo <udoeyo91@gmail.com>
2022-06-03 00:09:38 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
+int main(void)
 {
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
 	printf("Size of  char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
