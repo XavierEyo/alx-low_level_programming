9a8eae85e821b6f1c1e1cf5402fcd1ae1f8d8a7d
XavierEyo <udoeyo91@gmail.com>
2022-06-08 17:31:53 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,15 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
 	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n')
 }
