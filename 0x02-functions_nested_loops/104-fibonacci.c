baea7c54c2f2f5ec7468208a5aa3fa220a391093
XavierEyo <udoeyo91@gmail.com>
2022-06-09 08:26:00 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int count = 98, a = 1, b = 2, c, i;
 	printf ("First %d terms of fibonacci series:\n", count);
-	for (i = 1; i < count; i++)
+	for (i = 0; i < count; i++)
 	{
 		if (i >= 1)
 			c = i;
