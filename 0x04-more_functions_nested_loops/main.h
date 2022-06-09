b134b293d733d13476d14933ee9815d34b25ebfd
XavierEyo <udoeyo91@gmail.com>
2022-06-09 11:19:59 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * header file: main.h
+ */
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,3 +17,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
