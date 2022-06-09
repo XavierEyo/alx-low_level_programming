68d2c47098ddda7b25c809fd8edd9931830ac4f9
XavierEyo <udoeyo91@gmail.com>
2022-06-09 08:15:49 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count = 98, a = 1, b = 2, c, i;
+	printf ("First %d terms of fibonacci series:\n", count);
+	for (i = 0; i < count; i++)
+	{
+		if (i <= 1)
+			next_term = i;
+		else
+		{
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		printf("%d\n", c);
+	}
+
+	return 0;
+}
