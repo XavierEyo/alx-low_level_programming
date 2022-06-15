78f1a08581dece37978b87b266a19a3142ea626b
XavierEyo <udoeyo91@gmail.com>
2022-06-15 14:51:03 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,12 +14,11 @@ char *leet(char *s)
 
 	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j<= 9; j++)
 		{
-			if (a[j] == *(s + i))
+			if (a[j] == *(s+i))
 				*(s + i) = b[j];
 		}
-
 	}
 	return (s);
 }
