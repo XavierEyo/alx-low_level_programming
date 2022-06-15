6e7a3652b58953c2bbff0c3013317dfdf3e24b17
XavierEyo <udoeyo91@gmail.com>
2022-06-15 14:46:49 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *leet(char *);
+char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char);
