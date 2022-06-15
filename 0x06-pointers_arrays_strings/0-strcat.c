7055326e35cea222c08ca4ff53226c523ca4f0e6
XavierEyo <udoeyo91@gmail.com>
2022-06-15 13:57:47 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,7 +16,7 @@ char *_strcat(char *dest, char *src)
 
 	while (count2 >= 0)
 	{
-		*(dest + count) = *(src + count);
+		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
 		count++;
