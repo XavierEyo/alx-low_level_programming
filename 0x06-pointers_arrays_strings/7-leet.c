2dbfb007632f56d4dddce75ab9c2c8765c7a956c
XavierEyo <udoeyo91@gmail.com>
2022-06-15 15:02:50 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,22 @@
  */
 char *leet(char *s)
 {
-	int i, j;
-	char a[] = "aAeEoOTlL";
-	char b[] = "4433007711";
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 86};
+	int numbers[] = {52, 51, 48, 55, 49};
 
-	for (i = 0; *(s + i); i++)
+	while (*(s + count) != '\0')
 	{
-		for (j = 0; j<= 9; j++)
+		for (i = 0; i < 5; i++)
 		{
-			if (a[j] == *(s+i))
-				*(s + i) = b[j];
+			if (*(s + count) == low_letters[i] || *(s + count) == upo_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
 		}
+		count++;
 	}
 	return (s);
 }
