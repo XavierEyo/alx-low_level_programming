9baf8a2534463ce899506b30a9438bde8b620599
XavierEyo <udoeyo91@gmail.com>
2022-06-15 18:46:15 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,31 @@
-#include "main.h"
+#include "holberton.h"
+ /**
+  * cap_string - capitalizes all words of a string
+  * @s: input string.
+  * Return: the pointer to dest.
+  */
 
-/**
- * cap_string - capitalizes all words of a string
- * @str: The string to be capitalized
- * Return: a pointer to the changed string
- */
-char *cap_string(char *str)
-{
-	int index = 0;
+ char *cap_string(char *s)
+ {
+         int count = 0, i;
+         int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (str[index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0
-		    	str[index] -= 32:
-		index++;
-	}
-	return (str);
-}
+         if (*(s + count) >= 97 && *(s + count) <= 122)
+                 *(s + count) = *(s + count) - 32;
+         count++;
+         while (*(s + count) != '\0')
+         {
+                 for (i = 0; i < 13; i++)
+                 {
+                         if (*(s + count) == sep_words[i])
+                         {
+                                 if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+                                         *(s + (count + 1)) = *(s + (count + 1)) - 32;
+                                 break;
+                         }
+                 }
+                 count++;
+         }
+         return (s);
+ }
 
