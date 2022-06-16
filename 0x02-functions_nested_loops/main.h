de65d3a3e080e8e5f423e83a37e9d8f86b0f81fe
XavierEyo <udoeyo91@gmail.com>
2022-06-16 14:37:14 -0700

    header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,3 +11,8 @@ int print_sign(int n);
 int _isalpha(int c);
 int _abs(int);
 void print_times_table(int n);
+int print_last_digit(int);
+void jack_bauer(void);
+void print_to_98(int n);
+
+#endif
