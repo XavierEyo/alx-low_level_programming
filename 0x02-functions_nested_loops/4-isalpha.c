ba6fd51e56919613a7bb6b28896f57fafd97a920
XavierEyo <udoeyo91@gmail.com>
2022-06-16 14:51:09 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - shows 1 if input isna letter. Otherwise 0
+ *
+ * @c: character in ASCII code
+ *
+ * Return: 1 for letters. 0 for rest
+ */
+int _isalpha(int c)
+{
+	if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
