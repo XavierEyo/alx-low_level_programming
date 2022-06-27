3f353508dda64d4fed03fb9f0bc1b79f2ca49f32
XavierEyo <udoeyo91@gmail.com>
2022-06-27 15:04:18 -0700

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,11 +8,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, k , l;
+	unsigned int i, j, k, l;
 	char *s;
 
 	if (s1 == NULL)
 		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 = NULL)
+		j = 0;
 	else
 	{
 		for (j = 0; s2[j]; j++)
