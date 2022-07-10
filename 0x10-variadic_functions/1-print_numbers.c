96541a960dc97c70ecf9ae0b7f3966a23acdafcc
XavierEyo <udoeyo91@gmail.com>
2022-07-10 00:05:47 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nums, int));
-		if (separator && i < n -1)
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
